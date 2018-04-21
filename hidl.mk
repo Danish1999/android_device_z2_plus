0092357a2f131cbef746c748e76cd2750bcadb2d
root <root@shahzeb.c.evident-lock-201507.internal>
2018-04-21 07:42:26 +0000

    Revert "Zuk: fix slowness of Camera,Dt2w correct path"
    This reverts commit 908dd21b3dcc289869c8e8d49e1fb9b7c3a244e4.

--- hidl.mk
+++ hidl.mk
@@ -32,9 +32,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-     camera.device@3.2-impl
+    android.hardware.camera.provider@2.4-impl
 
 # Display
 PRODUCT_PACKAGES += \
